371fc5ee110f7d7111ed880673f5841e57ca0255
Asma <158836479+asma675@users.noreply.github.com>
2025-12-22 21:10:33 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
